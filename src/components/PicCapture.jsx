c43a6290d9915c8a1b04e9a7d9cd5fbf758eb67b
AdiGolandaj <adigolandaj786@gmail.com>
2025-02-10 00:37:31 +0530

    added images and got overlay working

--- src/components/PicCapture.jsx
+++ src/components/PicCapture.jsx
@@ -1,5 +1,7 @@
 import React, { useRef, useState } from 'react';
 import Webcam from 'react-webcam';
+import './pic_style.css'
+
 
 const PicCapture = () => {
   const webcamRef = useRef(null);
@@ -12,16 +14,35 @@ const PicCapture = () => {
 
   return (
     <div className='flex '>
+      <div className='flex flex-col'>
       <Webcam
         audio={false}
         ref={webcamRef}
         screenshotFormat="image/jpeg"
       />
       <button onClick={capture}>Capture photo</button>
+      </div>
       {imageSrc && (
         <div>
           <h2>Captured Image:</h2>
-          <img src={imageSrc} alt="Captured" />
+          <div className='absolute'>
+            <div style={
+              {
+                backgroundImage: `url(${imageSrc})`,
+                backgroundSize: 'cover',
+                backgroundPosition: 'center',
+                width: '40em',
+                height: '32em',
+                position: 'relative',
+                zIndex: 1,
+              }
+            }>
+              <div className='absolute top-4/5 left-1/2 transform -translate-x-1/2 -translate-y-1/2'>
+                <img src="public\necklace_1.png" alt="" />
+              </div>
+            </div>
+          {/* <img src={imageSrc} alt="Captured" /> */}
+          </div>
         </div>
       )}
     </div>
